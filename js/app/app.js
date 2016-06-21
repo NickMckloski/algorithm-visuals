0da978ce87167e8013a1d84f6a74264de4610304
Nick Mckloski <mckloski.nick@gmail.com>
2016-06-21 11:57:33 -0400

    bubble sort complete

--- js/app/app.js
+++ js/app/app.js
@@ -3,16 +3,24 @@ var app = angular.module('app', []);
 app.factory('Sorting', function ($window) {
     var root = {};
 
+    /**
+     * Insertion sort logic
+     * @constructor
+     */
     root.InsertionSort = function() {
         var alg = this;
 
         //start at -1 because it will increment to 0
         var i = -1;
 
+        //algorithm is run through an interval to slow it down
+        //and allow visual canvas the be drawn.
         var interval = setInterval(function() {
             i++;
             alg.Position = i;
             if(i >= alg.Collection.length) {
+                alg.Position = null;
+                alg.DrawCollection();
                 clearInterval(interval);
                 return;
             }
@@ -32,33 +40,51 @@ app.factory('Sorting', function ($window) {
 
     };
 
+    /**
+     * Bubble sort logic
+     * @constructor
+     */
     root.BubbleSort = function() {
         var alg = this;
 
         //start at -1 because it will increment to 0
         var i = -1;
+        var changesMade = true;
+        var lastUnsortedIndex = alg.Collection.length;
 
+        //algorithm is run through an interval to slow it down
+        //and allow visual canvas the be drawn.
         var interval = setInterval(function() {
             i++;
             alg.Position = i;
-            if(i >= alg.Collection.length) {
-                clearInterval(interval);
-                return;
+            if(i >= lastUnsortedIndex) {
+                if(!changesMade) {
+                    //end loop
+                    alg.Position = null;
+                    alg.DrawCollection();
+                    clearInterval(interval);
+                    return;
+                } else {
+                    //reset loop
+                    changesMade = false;
+                    lastUnsortedIndex = i - 1;
+                    i = 0;
+                }
             }
 
-            // for (var j = 0; j < (alg.Collection.length - i - 1); j++) { //Notice that j < (length - i)
-            //     //Compare the adjacent positions
-            //     if(alg.Collection[j] < alg.Collection[j+1]) {
-            //         //Swap the numbers
-            //         var tmp = alg.Collection[j];  //Temporary variable to hold the current number
-            //         alg.Collection[j] = alg.Collection[j+1]; //Replace current number with adjacent number
-            //         alg.Collection[j+1] = tmp; //Replace adjacent number with current number
-            //     }
-            // }
+            //compare elements
+            if(alg.Collection[i] < alg.Collection[i+1]) {
+                var temp = alg.Collection[i + 1];
+                //replace elements
+                alg.Collection[i + 1] = alg.Collection[i];
+                alg.Collection[i] = temp;
+                changesMade = true;
+            }
 
             alg.DrawCollection();
-        }, 50);
+        }, 15);
     };
+
 
     root.MergeSort = function() {
         console.log(this);
